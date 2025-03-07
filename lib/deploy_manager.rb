62b67e39913bd47df797757c821f2f84fa4fbaeb
Leva84 <denfrut8@gmail.com>
2025-03-07 22:09:29 +0300

    Deploy updated site

--- lib/deploy_manager.rb
+++ lib/deploy_manager.rb
@@ -30,7 +30,7 @@ class DeployManager
   # Step 1: Checking the current branch
   def check_main_branch
     if current_branch != 'main'
-      abort("Error: Deployment is allowed only from the 'main' branch. Current branch: '#{current_branch}'")
+      abort("Error: Deployment is allowed only from the 'main' branch. Current branch: '#{ current_branch }'")
     end
     logger.info "You are on the 'main' branch. Proceeding with deployment..."
   end
@@ -62,8 +62,8 @@ class DeployManager
 
   # Step 5: Push changes in the Main branch
   def push_changes_to_main
-    logger.info "Pushing changes to branch '#{current_branch}'..."
-    system("git push origin #{current_branch}") || abort('Error while pushing changes to main.')
+    logger.info "Pushing changes to branch '#{ current_branch }'..."
+    system("git push origin #{ current_branch }") || abort('Error while pushing changes to main.')
   end
 
   # Step 6: Deploy site in gh-pages
@@ -71,7 +71,6 @@ class DeployManager
     logger.info "Switching to the 'gh-pages' branch for deployment."
     ensure_branch_exists
     switch_to_gh_pages_branch
-    # copy_docs_folder
     commit_docs_changes
     push_gh_pages_changes
     switch_back_to_main_branch
@@ -95,29 +94,15 @@ class DeployManager
     system('git checkout gh-pages') || abort('Error while switching to gh-pages branch.')
   end
 
-  # def copy_docs_folder
-  #   logger.info "Copying new files to 'gh-pages/docs'..."
-  #   system('git checkout main -- docs') || abort('Error while copying docs folder.')
-  #   logger.info "Staging only changes from the 'docs' directory..."
-  #   system('git add docs') || abort('Error while staging docs folder.')
-  # end
-
   def commit_docs_changes
-    # logger.info "Committing changes to 'gh-pages'."
-    # system('git commit -m "Update docs in gh-pages"') ||
-    #   logger.info('No changes to commit, skipping...')
-
     logger.info "Checking for changes in the 'docs' directory..."
 
-    # Проверяем статус папки docs
     status = `git status --porcelain #{ output_dir }`.strip
 
     if status.empty?
       logger.info "No changes detected in the '#{ output_dir }' directory. Skipping commit."
     else
       logger.info "Changes detected in '#{ output_dir }'. Staging and committing..."
-
-      # Добавляем изменения в индекс и коммитим
       system("git add #{ output_dir }") || abort("Error while staging changes in '#{ output_dir }'.")
       system('git commit -m "Update docs in gh-pages"') || logger.info('No changes to commit, skipping...')
     end
@@ -129,7 +114,7 @@ class DeployManager
   end
 
   def switch_back_to_main_branch
-    logger.info "Switching back to '#{current_branch}' branch."
-    system("git checkout #{current_branch}") || abort('Error while switching back to main branch.')
+    logger.info "Switching back to '#{ current_branch }' branch."
+    system("git checkout #{ current_branch }") || abort('Error while switching back to main branch.')
   end
 end
