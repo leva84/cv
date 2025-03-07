8fe3cc5af6dacd2af0fbba11fb2b57f690624fc1
Leva84 <denfrut8@gmail.com>
2025-03-07 22:36:22 +0300

    Deploy updated site

--- lib/deploy_manager.rb
+++ lib/deploy_manager.rb
@@ -80,12 +80,7 @@ class DeployManager
     return if system('git show-ref --quiet refs/heads/gh-pages')
 
     logger.info "'gh-pages' branch does not exist. Creating it..."
-    system('git checkout --orphan gh-pages') ||
-      abort('Error while creating gh-pages branch.')
-    system('git rm -rf .') || abort('Error while clearing initial contents.')
-    system('git commit --allow-empty -m "Initialize gh-pages branch"') ||
-      abort('Error while initializing gh-pages branch.')
-    system('git push origin gh-pages') || abort('Error while pushing gh-pages branch.')
+    abort('Error while creating gh-pages branch.')
   end
 
   def switch_to_gh_pages_branch
@@ -108,7 +103,7 @@ class DeployManager
 
   def push_gh_pages_changes
     logger.info 'Pushing changes to origin/gh-pages.'
-    system('git push origin gh-pages') || abort('Error while pushing changes to gh-pages branch.')
+    system('git push -u origin gh-pages') || abort('Error while pushing changes to gh-pages branch.')
     logger.info 'Deployment to gh-pages completed successfully!'
   end
 
